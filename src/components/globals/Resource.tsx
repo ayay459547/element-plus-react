50f95ea9f4f136594bb4f2ffb972524d8e3cd8c3
ayay459547 <ayay459547@gmail.com>
2025-12-02 01:19:35 +0800

    feat: resource

--- src/components/globals/Resource.tsx
+++ src/components/globals/Resource.tsx
@@ -1,11 +1,104 @@
 import VPHeroContent from '@/components/VPHeroContent.tsx'
+import resourceLang from '@/crowdin/pages/resource.json'
+import ElCard from '@ayay/element-plus-react/components/card/ElCard.tsx'
 import styles from './Resource.module.scss'
 
+import AxureComponentsSvg from './resources/AxureComponentsSvg.tsx'
+import FigmaTemplateSvg from './resources/FigmaTemplateSvg.tsx'
+import FigmaUiKitSvg from './resources/FigmaUiKitSvg.tsx'
+import FigmaVariablesSvg from './resources/FigmaVariablesSvg.tsx'
+import MasterGoUiKitSvg from './resources/MasterGoUiKitSvg.tsx'
+import SketchTemplateSvg from './resources/SketchTemplateSvg.tsx'
+
+const mirrorUrl = 'element-plus.gitee.io'
+const isMirrorUrl = () => {
+  // if (!isClient) return
+  return window.location.hostname === mirrorUrl
+}
+const resourceUrl = {
+  github: {
+    sketch:
+      'https://github.com/ElementUI/Resources/raw/master/Element_Plus_Design_System_2022_1.0_Beta.zip',
+    axure: 'https://github.com/ElementUI/Resources/raw/master/Element_Components_v2.1.0.rplib'
+  },
+  gitee: {
+    sketch:
+      'https://gitee.com/element-plus/resources/raw/master/Element_Plus_Design_System_2022_1.0_Beta.zip',
+    axure: 'https://gitee.com/element-plus/resources/raw/master/Element_Components_v2.1.0.rplib'
+  }
+}[isMirrorUrl() ? 'gitee' : 'github']
+
+const resourceCards = [
+  {
+    key: '2024-master-go-ui-kit',
+    title: resourceLang.masterGo2024,
+    icon: <MasterGoUiKitSvg />,
+    intro: resourceLang.masterGo2024Intro,
+    url: 'https://mastergo.com/community/resource/124855257836266'
+  },
+  {
+    key: '2023-figma-ui-kit',
+    title: resourceLang.figma2023,
+    icon: <FigmaUiKitSvg />,
+    intro: resourceLang.figma2023Intro,
+    url: 'https://www.figma.com/community/file/1305760370797950824/element-plus-design-system-ui-kit'
+  },
+  {
+    key: 'figma-variables',
+    title: resourceLang.figmaVariables,
+    icon: <FigmaVariablesSvg />,
+    intro: resourceLang.figmaVariablesIntro,
+    url: 'https://www.figma.com/community/file/1256091634199852065'
+  },
+  {
+    key: 'figma',
+    title: resourceLang.figma,
+    icon: <FigmaTemplateSvg />,
+    intro: resourceLang.figmaIntro,
+    url: 'https://www.figma.com/community/file/1021254029764378306'
+  },
+  {
+    key: 'sketch',
+    title: resourceLang.sketch,
+    icon: <SketchTemplateSvg />,
+    intro: resourceLang.sketchIntro,
+    url: resourceUrl.sketch
+  },
+  {
+    key: 'axure',
+    title: resourceLang.axure,
+    icon: <AxureComponentsSvg />,
+    intro: resourceLang.axureIntro,
+    url: resourceUrl.axure
+  }
+]
+
 const Resource = () => {
+  const onClickHandler = (item: string) => {
+    console.log(item)
+    // sendEvent('resource_download', item)
+  }
+
   return (
     <VPHeroContent>
-      <div className={styles['resource']}>
-        <h1>Resource Component</h1>
+      <div className={styles['page-resource']}>
+        <h1>{resourceLang['title']}</h1>
+        <p>{resourceLang.lineOne}</p>
+        <div className={styles['resource-content']}>
+          {resourceCards.map((card) => {
+            return (
+              <ElCard key={card.title} className={styles['card']} shadow="hover">
+                <div className="w-30 m-auto">{card.icon}</div>
+                <h3>{card.title}</h3>
+                <p>{card.intro}</p>
+                <a target="_blank" href={card.url} onClick={() => onClickHandler(card.title)}>
+                  {/* <el-button type="primary">{ resourceLang.download }</el-button> */}
+                  <button className={styles['resource-btn']}>{resourceLang.download}</button>
+                </a>
+              </ElCard>
+            )
+          })}
+        </div>
       </div>
     </VPHeroContent>
   )
---
+++ src/components/globals/resources/SketchTemplateSvg.tsx
@@ -0,0 +1,53 @@
+import styles from './SketchTemplateSvg.module.scss'
+
+export default function SketchTemplateSvg() {
+  return (
+    <svg
+      id="图层_1"
+      version="1.1"
+      // xmlns="http://www.w3.org/2000/svg"
+      // xmlns:xlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 120 120"
+      // style="enable-background: new 0 0 120 120"
+      // xml:space="preserve"
+    >
+      <path
+        className={styles['st0']}
+        d="M13,15h48c3.3,0,6,2.7,6,6v33c0,3.3-2.7,6-6,6H13c-3.3,0-6-2.7-6-6V21C7,17.7,9.7,15,13,15z"
+      />
+      <path
+        className={styles['st0']}
+        d="M81,15h20c3.3,0,6,2.7,6,6v33c0,3.3-2.7,6-6,6H81c-3.3,0-6-2.7-6-6V21C75,17.7,77.7,15,81,15z"
+      />
+      <path
+        className={styles['st1']}
+        d="M12.5,15h49c3,0,5.5,2.5,5.5,5.5l0,0c0,3-2.5,5.5-5.5,5.5h-49c-3,0-5.5-2.5-5.5-5.5l0,0C7,17.5,9.5,15,12.5,15z
+      "
+      />
+      <path
+        className={styles['st2']}
+        d="M80.5,15h21c3,0,5.5,2.5,5.5,5.5l0,0c0,3-2.5,5.5-5.5,5.5h-21c-3,0-5.5-2.5-5.5-5.5l0,0
+      C75,17.5,77.5,15,80.5,15z"
+      />
+      <path
+        className={styles['st0']}
+        d="M90,68c3.3,0,6,2.7,6,6v28c0,2.2-1.8,4-4,4H13c-3.3,0-6-2.7-6-6V74c0-3.3,2.7-6,6-6H90z"
+      />
+      <path
+        className={styles['st2']}
+        d="M18,73.5v27c0,3-2.5,5.5-5.5,5.5l0,0c-3,0-5.5-2.5-5.5-5.5v-27c0-3,2.5-5.5,5.5-5.5l0,0
+      C15.5,68,18,70.5,18,73.5z"
+      />
+      <path className={styles['st3']} d="M84.3,78.7L79.5,68h13.1L84.3,78.7z" />
+      <path className={styles['st4']} d="M102.1,78.7H84.2L92.6,68L102.1,78.7z" />
+      <path className={styles['st5']} d="M70,78.7L79.5,68l4.8,10.7H70z" />
+      <path className={styles['st6']} d="M92.6,68l9.5,10.7l4.8-10.7H92.6z" />
+      <path className={styles['st5']} d="M106.8,68l8.3,10.7h-13.1L106.8,68z" />
+      <path className={styles['st7']} d="M93.8,106l21.4-27.3h-13.1L93.8,106z" />
+      <path className={styles['st3']} d="M70,78.7L93.8,106l-9.5-27.3H70z" />
+      <path className={styles['st5']} d="M93.8,106l8.3-27.3H84.2L93.8,106z" />
+    </svg>
+  )
+}
