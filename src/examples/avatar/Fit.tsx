0b4d4dc64b12acc40762d60068aac3e800dfb837
Caleb <ayay459547@gmail.com>
2025-12-26 14:02:27 +0800

    feat: Avatar Page

---
+++ src/examples/avatar/Fit.tsx
@@ -0,0 +1,23 @@
+import ElAvatar from '@ayay/element-plus-react/components/avatar/ElAvatar.tsx'
+import type { CSSProperties } from 'react'
+import styles from './Fit.module.scss'
+
+const AvatarFit: React.FC = () => {
+  const fits: CSSProperties['objectFit'][] = ['fill', 'contain', 'cover', 'none', 'scale-down']
+  const url = 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg'
+
+  return (
+    <div className={styles['demo-fit']}>
+      {fits.map((fit) => {
+        return (
+          <div key={fit} className={styles['block']}>
+            <span className={styles['title']}>{fit}</span>
+            <ElAvatar shape="square" size={100} fit={fit} src={url} />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default AvatarFit
--- src/pages/component/data/AvatarPage.tsx
+++ src/pages/component/data/AvatarPage.tsx
@@ -3,6 +3,8 @@ import VPDocContent from '@/components/VPDocContent.tsx'
 import VPDemo from '@/components/VPDemo.tsx'
 
 import AvatarBasic from '@/examples/avatar/Basic.tsx'
+import AvatarFallback from '@/examples/avatar/Fallback.tsx'
+import AvatarFit from '@/examples/avatar/Fit.tsx'
 import AvatarTypes from '@/examples/avatar/Types.tsx'
 
 const AvatarPage: React.FC = () => {
@@ -26,6 +28,35 @@ const AvatarPage: React.FC = () => {
       <p>It supports images, Icons, or characters.</p>
       <VPDemo>
         <AvatarTypes />
+      </VPDemo>
+
+      <h2 id="fallback">Fallback</h2>
+      <p>fallback when image load error.</p>
+      <VPDemo>
+        <AvatarFallback />
+      </VPDemo>
+
+      <h2 id="fit-container">Fit Container</h2>
+      <p>
+        Set how the image fit its container for an image avatar, same as{' '}
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/CSS/object-fit"
+          className="vp-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          object-fit
+          <svg viewBox="0 0 24 24" width="1.2em" height="1.2em" className="link-icon">
+            <path
+              fill="currentColor"
+              d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1zm11-3v8h-2V6.413l-7.793 7.794l-1.414-1.414L17.585 5H13V3z"
+            ></path>
+          </svg>
+        </a>
+        .
+      </p>
+      <VPDemo>
+        <AvatarFit />
       </VPDemo>
     </VPDocContent>
   )
