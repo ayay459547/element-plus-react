8d396951741d83f54977e5549cb2a57ec43b4401
Caleb <ayay459547@gmail.com>
2026-02-04 16:43:33 +0800

    feat: components use forwardRef

--- packages/components/row/ElRow.tsx
+++ packages/components/row/ElRow.tsx
@@ -2,7 +2,7 @@ import type { ElColProps } from '@ayay459547/element-plus-react/components/col/t
 import { addUnit } from '@ayay459547/element-plus-react/utils/dom/style'
 import clsx from 'clsx'
 import type { CSSProperties } from 'react'
-import { Children, cloneElement, isValidElement } from 'react'
+import { Children, cloneElement, forwardRef, isValidElement } from 'react'
 import styles from './ElRow.module.scss'
 import type { ElRowProps } from './types'
 import { RowContext } from './useRow'
@@ -11,56 +11,56 @@ function isElCol(element: any): element is React.ReactElement<ElColProps> {
   return element?.type !== undefined
 }
 
-const ElRow = <T extends React.ElementType = 'div'>({
-  gutter = 0,
-  justify = 'start',
-  align,
-  tag,
-  children,
-  className,
-  style,
-  ...props
-}: ElRowProps<T>) => {
-  const Tag = tag || 'div'
+const COMPONENT_NAME = 'ElRow'
 
-  let gutterX: ReturnType<typeof addUnit> = ''
-  const rowStyles: CSSProperties = {}
+const ElRow = forwardRef<HTMLElement, ElRowProps<any>>(
+  ({ gutter = 0, justify = 'start', align, tag, children, className, style, ...props }, ref) => {
+    const Tag = tag || 'div'
 
-  if (gutter) {
-    gutterX = addUnit(gutter / 2)
-    rowStyles.marginRight = `-${gutterX}`
-    rowStyles.marginLeft = `-${gutterX}`
+    let gutterX: ReturnType<typeof addUnit> = ''
+    const rowStyles: CSSProperties = {}
+
+    if (gutter) {
+      gutterX = addUnit(gutter / 2)
+      rowStyles.marginRight = `-${gutterX}`
+      rowStyles.marginLeft = `-${gutterX}`
+    }
+
+    return (
+      <RowContext.Provider value={{ gutter }}>
+        <Tag
+          {...props}
+          ref={ref}
+          className={clsx(
+            'el-row',
+            styles['el-row'],
+            align ? styles[`is-align-${align}`] : '',
+            justify !== 'start' ? styles[`is-justify-${justify}`] : '',
+            className
+          )}
+          style={{
+            ...rowStyles,
+            ...style
+          }}
+        >
+          {Children.map(children, (child) => {
+            if (!isValidElement(child)) return child
+
+            if (!isElCol(child)) return child
+
+            return cloneElement(child, {
+              // 不覆蓋 children 已經傳入的 props
+              ...child.props
+            })
+          })}
+        </Tag>
+      </RowContext.Provider>
+    )
   }
+)
 
-  return (
-    <RowContext.Provider value={{ gutter }}>
-      <Tag
-        {...props}
-        className={clsx(
-          'el-row',
-          styles['el-row'],
-          align ? styles[`is-align-${align}`] : '',
-          justify !== 'start' ? styles[`is-justify-${justify}`] : '',
-          className
-        )}
-        style={{
-          ...rowStyles,
-          ...style
-        }}
-      >
-        {Children.map(children, (child) => {
-          if (!isValidElement(child)) return child
+ElRow.displayName = COMPONENT_NAME
 
-          if (!isElCol(child)) return child
-
-          return cloneElement(child, {
-            // 不覆蓋 children 已經傳入的 props
-            ...child.props
-          })
-        })}
-      </Tag>
-    </RowContext.Provider>
-  )
-}
+ElRow.displayName = COMPONENT_NAME
 
 export default ElRow
--- packages/components/text/ElText.tsx
+++ packages/components/text/ElText.tsx
@@ -1,48 +1,61 @@
 import clsx from 'clsx'
-import type { CSSProperties, ElementType } from 'react'
+import type { CSSProperties } from 'react'
+import { forwardRef } from 'react'
 import styles from './ElText.module.scss'
 import type { ElTextProps } from './types'
 
-const ElText = <T extends ElementType = 'span'>({
-  type,
-  size = 'default',
-  truncated = false,
-  lineClamp,
-  tag,
-  children,
-  className,
-  style = {},
-  ...rest
-}: ElTextProps<T>) => {
-  const Tag = tag || 'span'
+const COMPONENT_NAME = 'ElText'
 
-  const mergedStyle: CSSProperties = {
-    ...(lineClamp
-      ? {
-          lineClamp: lineClamp,
-          WebkitLineClamp: lineClamp
-        }
-      : {}),
-    ...style
+const ElText = forwardRef<HTMLElement, ElTextProps<any>>(
+  (
+    {
+      type,
+      size = 'default',
+      truncated = false,
+      lineClamp,
+      tag,
+      children,
+      className,
+      style = {},
+      ...rest
+    },
+    ref
+  ) => {
+    const Tag = tag || 'span'
+
+    const mergedStyle: CSSProperties = {
+      ...(lineClamp
+        ? {
+            lineClamp: lineClamp,
+            WebkitLineClamp: lineClamp
+          }
+        : {}),
+      ...style
+    }
+
+    return (
+      <Tag
+        {...rest}
+        ref={ref}
+        className={clsx(
+          'el-text',
+          styles['el-text'],
+          styles[`el-text--${type}`],
+          styles[`el-text--${size}`],
+          truncated ? styles['is-truncated'] : '',
+          lineClamp ? styles['is-line-clamp'] : '',
+          className
+        )}
+        style={mergedStyle}
+      >
+        {children}
+      </Tag>
+    )
   }
+)
 
-  return (
-    <Tag
-      {...rest}
-      className={clsx(
-        'el-text',
-        styles['el-text'],
-        styles[`el-text--${type}`],
-        styles[`el-text--${size}`],
-        truncated ? styles['is-truncated'] : '',
-        lineClamp ? styles['is-line-clamp'] : '',
-        className
-      )}
-      style={mergedStyle}
-    >
-      {children}
-    </Tag>
-  )
-}
+ElText.displayName = COMPONENT_NAME
+
+ElText.displayName = COMPONENT_NAME
 
 export default ElText
