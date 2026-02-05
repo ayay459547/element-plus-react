ff2c5fbc1a292cc79b5ed932863381773f85eaf5
Caleb <ayay459547@gmail.com>
2026-02-05 16:00:18 +0800

    fix: examples ElTooltip

--- src/examples/tooltip/Basic.tsx
+++ src/examples/tooltip/Basic.tsx
@@ -7,114 +7,54 @@ const TooltipBasic: React.FC = () => {
   return (
     <div className={styles['tooltip-base-box']}>
       <div className={clsx(styles['row'], styles['center'])}>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Top Left prompts info"
-          placement="top-start"
-        >
+        <ElTooltip effect="dark" content="Top Left prompts info" placement="top-start">
           <ElButton>top-start</ElButton>
         </ElTooltip>
 
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Top Center prompts info"
-          placement="top"
-        >
+        <ElTooltip effect="dark" content="Top Center prompts info" placement="top">
           <ElButton>top</ElButton>
         </ElTooltip>
 
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Top Right prompts info"
-          placement="top-end"
-        >
+        <ElTooltip effect="dark" content="Top Right prompts info" placement="top-end">
           <ElButton>top-end</ElButton>
         </ElTooltip>
       </div>
 
       <div className={styles['row']}>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Left Top prompts info"
-          placement="left-start"
-        >
+        <ElTooltip effect="dark" content="Left Top prompts info" placement="left-start">
           <ElButton>left-start</ElButton>
         </ElTooltip>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Right Top prompts info"
-          placement="right-start"
-        >
+        <ElTooltip effect="dark" content="Right Top prompts info" placement="right-start">
           <ElButton>right-start</ElButton>
         </ElTooltip>
       </div>
 
       <div className={styles['row']}>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Left Center prompts info"
-          placement="left"
-        >
+        <ElTooltip effect="dark" content="Left Center prompts info" placement="left">
           <ElButton className="mt-3 mb-3">left</ElButton>
         </ElTooltip>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Right Center prompts info"
-          placement="right"
-        >
+        <ElTooltip effect="dark" content="Right Center prompts info" placement="right">
           <ElButton className="mt-3 mb-3">right</ElButton>
         </ElTooltip>
       </div>
 
       <div className={styles['row']}>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Left Bottom prompts info"
-          placement="left-end"
-        >
+        <ElTooltip effect="dark" content="Left Bottom prompts info" placement="left-end">
           <ElButton>left-end</ElButton>
         </ElTooltip>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Right Bottom prompts info"
-          placement="right-end"
-        >
+        <ElTooltip effect="dark" content="Right Bottom prompts info" placement="right-end">
           <ElButton>right-end</ElButton>
         </ElTooltip>
       </div>
 
       <div className={clsx(styles['row'], styles['center'])}>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Bottom Left prompts info"
-          placement="bottom-start"
-        >
+        <ElTooltip effect="dark" content="Bottom Left prompts info" placement="bottom-start">
           <ElButton>bottom-start</ElButton>
         </ElTooltip>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Bottom Center prompts info"
-          placement="bottom"
-        >
+        <ElTooltip effect="dark" content="Bottom Center prompts info" placement="bottom">
           <ElButton>bottom</ElButton>
         </ElTooltip>
-        <ElTooltip
-          className={styles['box-item']}
-          effect="dark"
-          content="Bottom Right prompts info"
-          placement="bottom-end"
-        >
+        <ElTooltip effect="dark" content="Bottom Right prompts info" placement="bottom-end">
           <ElButton>bottom-end</ElButton>
         </ElTooltip>
       </div>
