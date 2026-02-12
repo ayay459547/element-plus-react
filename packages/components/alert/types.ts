b697cd775e2db44427537aa0dba8f498962fb0dc
Caleb <ayay459547@gmail.com>
2026-02-12 14:16:24 +0800

    style: alert type format

--- packages/components/alert/types.ts
+++ packages/components/alert/types.ts
@@ -5,34 +5,42 @@ export interface ElAlertProps {
    * @description alert title.
    */
   title?: ReactNode | string
+
   /**
    * @description	alert type.
    */
   type?: 'primary' | 'success' | 'warning' | 'info' | 'error'
+
   /**
    * @description descriptive text.
    */
   description?: string
+
   /**
    * @description whether alert can be dismissed.
    */
   closable?: boolean
+
   /**
    * @description whether content is placed in the center.
    */
   center?: boolean
+
   /**
    * @description customized close button text.
    */
   closeText?: string
+
   /**
    * @description whether a type icon is displayed.
    */
   showIcon?: boolean
+
   /**
    * @description content of the alert icon.
    */
   icon?: ReactNode
+
   /**
    * @description theme style.
    */
